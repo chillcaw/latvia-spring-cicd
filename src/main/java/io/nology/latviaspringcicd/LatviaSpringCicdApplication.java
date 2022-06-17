2c3a2416cc6e9ca1db6aa36bfc3c0c54b3c52148
Calum Hill-Cawthorne <calum@nology.io>
2022-06-17 10:40:33 +1000

    fixed indentation, removed tabs

--- src/main/java/io/nology/latviaspringcicd/LatviaSpringCicdApplication.java
+++ src/main/java/io/nology/latviaspringcicd/LatviaSpringCicdApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LatviaSpringCicdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LatviaSpringCicdApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LatviaSpringCicdApplication.class, args);
+  }
 
 }
